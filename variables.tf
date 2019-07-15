58ba42162ca713c969a34c2bce1fc18e423aaedb
Kit Jonathan Te <kjonathante@gmail.com>
2019-07-15 09:55:17 -0700

    Commented out the optional type attribute that's showing as an error in variable.tf server_port.

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "secret_key" {
 }
 
 variable "server_port" {
-  type        = number
+  #type        = number
   description = "The port the server will use for HTTP requests"
   default     = 8080
 }
